60729c262ea90234473fcb4f5d2b5b838302e1fd
Sahil_thakur <96003711+ALLLENX@users.noreply.github.com>
2025-11-04 11:55:30 +0530

    Implement Fibonacci series in C++

---
+++ fibonacciSeries.cpp
@@ -0,0 +1,24 @@
+# include<bits/stdc++.h>
+using namespace std;
+
+int fib(int n)
+{
+    if(n<=1) return n;
+    
+    else
+    return fib(n-1)+fib(n-2);
+}
+int main()
+{
+    int n ;
+    cout<<"fibonacci series upto ?";
+    cin>>n;
+    
+    for(int i = 0;i<=n;i++)
+    {
+        cout<<fib(i)<<" ";//calling fib function 
+    }
+    cout<<endl;
+    
+    return 0;
+}
