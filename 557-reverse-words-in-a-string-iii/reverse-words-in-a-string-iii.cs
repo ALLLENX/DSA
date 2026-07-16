3fb81d600f3919694ba31c9b75469fc8dc271d00
Sahil_thakur <96003711+ALLLENX@users.noreply.github.com>
2026-07-16 19:58:49 +0530

    Time: 3 ms (75.24%) | Memory: 47.7 MB (80.00%) - LeetSync

---
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cs
@@ -0,0 +1,34 @@
+public class Solution
+{
+    public string ReverseWords(string s)
+    {
+        char[] chars = s.ToCharArray();
+
+        int start = 0;
+
+        for (int i = 0; i <= chars.Length; i++)
+        {
+            // End of a word
+            if (i == chars.Length || chars[i] == ' ')
+            {
+                Reverse(chars, start, i - 1);
+                start = i + 1;
+            }
+        }
+
+        return new string(chars);
+    }
+
+    private void Reverse(char[] chars, int left, int right)
+    {
+        while (left < right)
+        {
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}
